0594d75e5ae14f1cebc7ff92bd06842dedd27dea
Esmeralda Robledo <esme_sahori@hotmail.com>
2021-09-17 18:56:59 -0700

    .

---
+++ main.js
@@ -0,0 +1,18 @@
+var itinerario =[];
+
+window.onload = () => {
+    console.log(itinerario)
+}
+
+function addTask(tarea){
+    itinerario.push(tarea)
+    return itinerario
+}
+
+function editTask(){
+
+}
+
+function removeTask(){
+
+}
